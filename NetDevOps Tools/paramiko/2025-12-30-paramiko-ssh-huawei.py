2391196baf9d977137a2f0a012906c8c93cc7369
Alexmaoqiming <qimingmao0@gmail.com>
2026-02-02 00:19:24 +0800

    rename

---
+++ NetDevOps Tools/paramiko/2025-12-30-paramiko-ssh-huawei.py
@@ -0,0 +1,33 @@
+import paramiko
+import time
+
+ip = '192.168.1.110'
+username = 'sshadmin'
+password = 'huawei'
+
+# 创建paramiko的SSH对象
+ssh_client = paramiko.SSHClient()
+# 创建秘钥
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# 连接SSH
+ssh_client.connect(hostname=ip, username=username, password=password)
+print("Successfully connected to " + ip)
+# 唤醒shell
+command = ssh_client.invoke_shell()
+
+command.send("sys\n")
+time.sleep(1)
+command.send("int loop 1\n")
+command.send("ip add 1.1.1.1 24\n")
+time.sleep(1)
+command.send("return\n")
+
+command.send("save\n")
+command.send("y\n")
+time.sleep(1)
+
+output = command.recv(65535)
+print(output.decode("ascii"))
+
+ssh_client.close()
+
