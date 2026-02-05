3b35914ca2145a85a656a30c12187b96df0185c2
Alexmaoqiming <qimingmao0@gmail.com>
2026-02-05 14:31:32 +0800

    Update 2025-12-30-paramiko-ssh-huawei.py

--- NetDevOps Tools v1.0/paramiko/2025-12-30-paramiko-ssh-huawei.py
+++ NetDevOps Tools v1.0/paramiko/2025-12-30-paramiko-ssh-huawei.py
@@ -1,33 +1,34 @@
 import paramiko
 import time
 
-ip = '192.168.1.110'
+ip = '192.168.93.101'
 username = 'sshadmin'
-password = 'huawei'
+password = 'Lanfei_212'
 
 # 创建paramiko的SSH对象
 ssh_client = paramiko.SSHClient()
 # 创建秘钥
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # 连接SSH
-ssh_client.connect(hostname=ip, username=username, password=password)
+ssh_client.connect(hostname=ip, username=username, password=password, look_for_keys=False, allow_agent=False)
+
 print("Successfully connected to " + ip)
 # 唤醒shell
 command = ssh_client.invoke_shell()
 
+# 发送华为配置命令
 command.send("sys\n")
 time.sleep(1)
+# 配置一个环回接口
 command.send("int loop 1\n")
 command.send("ip add 1.1.1.1 24\n")
 time.sleep(1)
-command.send("return\n")
-
+command.send("quit\n")
+time.sleep(1)
 command.send("save\n")
 command.send("y\n")
-time.sleep(1)
 
 output = command.recv(65535)
 print(output.decode("ascii"))
 
 ssh_client.close()
-
