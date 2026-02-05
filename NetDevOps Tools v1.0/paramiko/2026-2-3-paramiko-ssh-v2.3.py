c36579137f6dda41b83fe1be71011c51f6c1f76a
Alexmaoqiming <qimingmao0@gmail.com>
2026-02-05 16:59:22 +0800

    抽象方法

--- NetDevOps Tools v1.0/paramiko/2026-2-3-paramiko-ssh-v2.3.py
+++ NetDevOps Tools v1.0/paramiko/2026-2-3-paramiko-ssh-v2.3.py
@@ -2,7 +2,19 @@ import paramiko
 import time
 import re
 
-
+"""
+V2.3版本改进（思科版本），将SSH连接执行命令的操作抽象为一个函数
+# 1. 建立SSH连接
+# 2. 打开交互式shell通道
+# 3. 丢弃欢迎信息
+# 4. 关闭分页
+# 5. 循环执行每条命令
+#    - 发送命令
+#    - 智能读取输出直到出现提示符
+#    - 清理输出（去回显、去提示符）
+# 6. 返回 {命令: 干净输出} 的字典
+# 7. 异常处理 + 资源清理
+"""
 def ssh_cisco_interactive(
         host: str,
         username: str,
@@ -20,18 +32,20 @@ def ssh_cisco_interactive(
             port=port,
             username=username,
             password=password,
-            look_for_keys=False,
+            look_for_keys=False, #禁用公钥认证和ssh-agent，只用密码
             allow_agent=False,
-            timeout=8
+            timeout=8    # 连接建立的超时
         )
 
+        # 打开交互式shell通道
         channel = ssh.invoke_shell()
         channel.settimeout(1.0)
 
+        # 丢弃初始欢迎信息 / banner / motd
         time.sleep(0.8)           # 等 banner / motd
         _ = channel.recv(8192)    # 丢弃欢迎信息
 
-        # 关闭分页（Cisco 必须）
+        # Cisco关闭分页
         channel.send("terminal length 0\n")
         time.sleep(0.4)
         _ = channel.recv(4096)
@@ -43,19 +57,23 @@ def ssh_cisco_interactive(
         # time.sleep(0.5)
         # _ = channel.recv(4096)
 
+        # 核心循环：逐条执行命令
         outputs = {}
 
         for cmd in commands:
+            # 清理命令两端空格
             cmd = cmd.strip()
+            # 跳过空命令
             if not cmd:
                 continue
-
+            # 发送命令 + 回车
             channel.send(cmd + "\n")
             print(f"发送: {cmd}")
 
             output = ""
             start = time.time()
 
+            # 加入时间判断，超出timeout自动跳出，防止死循环
             while time.time() - start < timeout:
                 if channel.recv_ready():
                     chunk = channel.recv(8192).decode('utf-8', errors='replace')
@@ -108,8 +126,8 @@ if __name__ == '__main__':
     ]
 
     ssh_cisco_interactive(
-        host="192.168.93.100",
+        host="192.168.93.101",
         username="sshadmin",
-        password="lanfei_242",
+        password="lanfei_ocsic",
         commands=cmds
     )
